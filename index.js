7c1a0892d31ec4de47eb31fb6850d76ea5a1274c
wangpeng <summer.wang@puman.com>
2019-01-05 22:35:24 +0800

    使用ajax请求

--- index.js
+++ index.js
@@ -12,7 +12,21 @@ app.get('/form', (req, res, next) => {
 app.post('/upload', upload.single('file'), (req, res, next) => {
   // req.file 是 `avatar` 文件的信息
   // req.body 将具有文本域数据，如果存在的话
+  res.set('Content-Type', 'application/json')
   res.send(req.file)
+})
+
+app.get('/preview/:filename',(req, res, next) => {
+  const options = {
+    root: __dirname + '/uploads/',
+    dotfiles: 'deny',
+    headers: {
+      'Content-Type': 'image/jpeg',
+    },
+  }
+  res.sendfile(req.params.filename, options, err => {
+    console.log(err);
+  })
 })
 
 app.listen(3000)
